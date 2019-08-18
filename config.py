18c4d594b0bbe990d48a910d77ed0cc9c2dd8041
yusanshi <yulei2018@gmail.com>
2019-08-18 19:09:52 +0800

    update parameters

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 MODEL_PATH = 'saved_model'
-DATA_NUM = 500
+DATA_NUM = None # None for all data
 SEQ_LENGTH = 100
-EPOCHS = 50
+EPOCHS = 100
 BATCH_SIZE = 64
-EMBEDDING_DIM = 256
+EMBEDDING_DIM = 512
 RNN_UNITS = 1024
 LEARNING_RATE = 0.001
-TEMPERATURE = 0.6  # bigger value means bigger randomness, and vice versa
+TEMPERATURE = 0.8  # bigger value means bigger randomness, and vice versa
