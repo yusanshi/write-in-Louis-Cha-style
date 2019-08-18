afb8e6941b23b73eeb055811d3a157e344928afd
yusanshi <yulei2018@gmail.com>
2019-08-19 00:55:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 MODEL_PATH = 'saved_model'
-DATA_NUM = None # None for all data
+CHECKPOINT_PATH = 'checkpoint'
+DATA_NUM = 500  # None for all data
 SEQ_LENGTH = 100
-EPOCHS = 100
+EPOCHS = 20
 BATCH_SIZE = 64
 EMBEDDING_DIM = 512
 RNN_UNITS = 1024
